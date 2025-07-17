a145b2756d80fe4471dfa955567409bf3c459573
NBKUSA <sridevinukella@gmail.com>
2025-07-18 02:16:31 +0530

    Add files via upload

---
+++ crypto.js
@@ -0,0 +1,5 @@
+module.exports = {
+    TRON_NODE: process.env.TRON_NODE || 'https://api.trongrid.io',
+    TRON_PRIVATE_KEY: process.env.TRON_PRIVATE_KEY,
+    TRON_WALLET: process.env.TRON_WALLET
+};
---
+++ iso8583.js
@@ -0,0 +1,4 @@
+module.exports = {
+    ISO_HOST: process.env.ISO_HOST || '127.0.0.1',
+    ISO_PORT: process.env.ISO_PORT || 5001
+};
